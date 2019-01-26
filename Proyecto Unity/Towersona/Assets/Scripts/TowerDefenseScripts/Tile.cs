57588ac448d276385fa967593af9847c6187315f
Pablo Martínez Domingo <pablokzx@gmail.com>
2019-01-26 01:03:47 +0100

    Generación del mundo y generación procedural del camino
    Soy la ostia

---
+++ Proyecto Unity/Towersona/Assets/Scripts/TowerDefenseScripts/Tile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public bool isPath = false;
+    public Vector2 position;
+
+    [SerializeField]
+    private Material pathMaterial;
+
+    public void MakePath()
+    {
+        isPath = true;
+        GetComponent<MeshRenderer>().material = pathMaterial;
+    }
+}
