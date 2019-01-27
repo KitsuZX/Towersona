f165ff26583b05144d3c0c343a27150784f80933
Aitor Iribar <aitor.iribar@gmail.com>
2019-01-27 10:53:55 +0100

    Corazones

---
+++ Proyecto Unity/Towersona/Assets/Scripts/HeartParticles.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartParticles : MonoBehaviour
+{
+    public ParticleSystem[] systems;
+
+    private bool isActive;
+
+    public void Play()
+    {
+        if (isActive) return;
+
+        for (int i = 0; i < systems.Length; i++)
+        {
+            systems[i].Play();
+        }
+
+        isActive = true;
+    }
+
+    public void Stop()
+    {
+        if (!isActive) return;
+
+        for (int i = 0; i < systems.Length; i++)
+        {
+            systems[i].Stop();
+        }
+
+        isActive = false;
+    }
+}
