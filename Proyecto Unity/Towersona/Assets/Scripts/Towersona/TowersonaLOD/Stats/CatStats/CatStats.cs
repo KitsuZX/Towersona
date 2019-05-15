6f06fb1614bfc1d604d412701cc4404571808414
Pablo Martínez Domingo <pablokzx@gmail.com>
2019-05-15 11:23:13 +0200

    Stats de cosas

---
+++ Proyecto Unity/Towersona/Assets/Scripts/Towersona/TowersonaLOD/Stats/CatStats/CatStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Stats", menuName = "Stats/Cat/Cat")]
+public class CatStats : ScriptableObject
+{
+    public Vector2 dañoBala;
+    public Vector2 velocidadDeAtaque;
+    public Vector2 rango;
+    public Vector2 velocidadBala;
+
+    public Vector2 dineroPorSegundo;
+
+    [Tooltip("Cuanta felicidad gana la towersona por caricia")]
+    public float felicidadPorCaricia;
+    [Tooltip("Hambre que pierde por segundo la towersona")]
+    public float hambrePorSegundo;
+    [Tooltip("Amor que pierde por segundo la towersona")]
+    public float perdidaFelicidadPorSegundo;
+    [Tooltip("Cuanto cae el amor por cada mierda que hay sin recoger")]
+    public float enfadoPorMierda;
+    public float tiempoEntreMierdas;
+    public int maxMierdas;
+}
---
+++ Proyecto Unity/Towersona/Assets/Scripts/Towersona/TowersonaLOD/Stats/CatStats/TigerStats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Stats", menuName = "Stats/Cat/Tiger")]
+public class TigerStats : ScriptableObject
+{
+    public Vector2 dañoBala;
+    public Vector2 velocidadDeAtaque;
+    public Vector2 rango;
+    public Vector2 velocidadBala;
+
+    public Vector2 dineroPorSegundo;
+    public Vector2 tamañoAreaDañoBala;
+
+    [Tooltip("Cuanta felicidad gana la towersona por caricia")]
+    public float felicidadPorCaricia;
+    [Tooltip("Hambre que pierde por segundo la towersona")]
+    public float hambrePorSegundo;
+    [Tooltip("Amor que pierde por segundo la towersona")]
+    public float perdidaFelicidadPorSegundo;
+    [Tooltip("Cuanto cae el amor por cada mierda que hay sin recoger")]
+    public float enfadoPorMierda;
+    public float tiempoEntreMierdas;
+    public int maxMierdas;
+}
