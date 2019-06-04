22f25817e911c908813f6fb622d21e6c36897906
Pablo Martínez Domingo <pablokzx@gmail.com>
2019-06-04 20:51:03 +0200

    Tiger Attack

---
+++ Proyecto Unity/Towersona/Assets/Towersonas/Cat/LVL 3 - 2 (TIGER)/Scripts/TigerAttack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TigerAttack : CatAttack
+{
+	TigerStats tigerStats;
+	private void Start()
+	{
+		base.Start();
+		tigerStats = (TigerStats)stats;	
+	}
+
+	public override void Shoot(Transform target)
+	{
+		GameObject bulletObject = Instantiate(bulletPrefab, towersonaLOD.firePoint.position, towersonaLOD.firePoint.rotation);
+		bulletObject.transform.SetParent(GameObject.FindGameObjectWithTag("Bullets Parent").transform, true);
+
+		TigerBullet bullet = bulletObject.GetComponent<TigerBullet>();
+		bullet.damage = tigerStats.currentAttackStrength;
+		bullet.speed = tigerStats.currentBulletSpeed;
+		bullet.explosionRadius = tigerStats.currentDamageArea;
+
+		if (bullet != null) bullet.Seek(target);
+	}	
+}
