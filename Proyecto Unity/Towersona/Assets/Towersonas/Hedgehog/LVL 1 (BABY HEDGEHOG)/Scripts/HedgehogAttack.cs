b4109699d3c45cf8333a6e7da07c3260d27106f7
Pablo Martínez Domingo <pablokzx@gmail.com>
2019-06-07 10:05:08 +0200

    Erizo terminado

---
+++ Proyecto Unity/Towersona/Assets/Towersonas/Hedgehog/LVL 1 (BABY HEDGEHOG)/Scripts/HedgehogAttack.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HedgehogAttack : AttackPattern
+{
+	HedgehogStats hedgehogStats;
+	private void Start()
+	{
+		Initialize();
+	}
+
+	protected void Initialize()
+	{
+		base.Start();
+		hedgehogStats = (HedgehogStats)stats;
+
+	}
+		
+
+	public override void Shoot(Transform target)
+	{
+		GameObject bulletObject = Instantiate(bulletPrefab, towersonaLOD.firePoint.position, towersonaLOD.firePoint.rotation);
+		bulletObject.transform.SetParent(GameObject.FindGameObjectWithTag("Bullets Parent").transform, true);
+		Bullet bullet = bulletObject.GetComponent<Bullet>();
+		bullet.damage = stats.currentAttackStrength;
+		bullet.speed = stats.currentBulletSpeed;
+
+		if (bullet != null) bullet.Seek(target);
+	}
+
+	public override void UpdateTarget()
+	{
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		float shortestDistance = Mathf.Infinity;
+		GameObject nearestEnemy = null;
+
+		foreach (GameObject enemy in enemies)
+		{
+			float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+			if (distanceToEnemy < shortestDistance)
+			{
+				shortestDistance = distanceToEnemy;
+				nearestEnemy = enemy;
+			}
+		}
+
+		if (nearestEnemy != null && shortestDistance <= stats.currentAttackRange)
+		{
+			target = nearestEnemy.transform;
+		}
+		else
+		{
+			target = null;
+		}
+	}	
+}
