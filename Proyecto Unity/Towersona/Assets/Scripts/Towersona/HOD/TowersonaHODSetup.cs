b5193550b552567323d7548ba35e5e0e29750bac
Aitor Iribar <aitor.iribar@gmail.com>
2019-10-23 15:40:08 +0200

    Arregladas referncias otra vez. Algo me está yendo raro con los GUIDs

--- Proyecto Unity/Towersona/Assets/Scripts/Towersona/HOD/TowersonaHODSetup.cs
+++ Proyecto Unity/Towersona/Assets/Scripts/Towersona/HOD/TowersonaHODSetup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using NaughtyAttributes;
 
 #pragma warning disable 649
 public class TowersonaHODSetup : MonoBehaviour
 {
-    [SerializeField] private Transform towersonaHODParent;
+    [SerializeField, Required] private Transform towersonaHODParent;
+    [SerializeField, Required] private LoveNeedUI loveNeedUI;
 
     /// <summary>
     /// Spawns the model in the Towersona Detailed Scene. The Detailed scene must have been alredy created.
@@ -16,9 +18,13 @@ public class TowersonaHODSetup : MonoBehaviour
         //Instantiate the towersona HOD
         GameObject towersonaHodGO = Instantiate(towersonaHODPrefab, towersonaHODParent, false);
         TowersonaNeeds needs = towersonaHodGO.GetComponent<TowersonaNeeds>();
-
+        
+        //Set the stats
         needs.SetStats(towersona.stats);
         needs.ResetNeeds();
+
+        //Hook up the UI
+        loveNeedUI.SetWatchedLoveNeed(needs.LoveNeed);
 
         return needs;
     }    
