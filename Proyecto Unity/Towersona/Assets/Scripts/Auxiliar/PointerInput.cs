d5103e32663105be8a93d74c7f73023569f66e23
Aitor Iribar <aitor.iribar@gmail.com>
2019-10-23 19:36:43 +0200

    Mejorado Draggable. Añadido PointerInput para abstraer el rollo de #if UNITY_EDITOR para táctil y ratón

--- Proyecto Unity/Towersona/Assets/Scripts/Auxiliar/PointerInput.cs
+++ Proyecto Unity/Towersona/Assets/Scripts/Auxiliar/PointerInput.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PointerInput : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public static class PointerInput
+{ 
+    public static bool IsTouching
     {
-        
+        get
+        {
+#if UNITY_EDITOR
+            return Input.touchCount > 0 || Input.GetMouseButton(0);
+#else
+            return Input.touchCount > 0;
+#endif
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public static bool ReadPosition(out Vector2 position)
     {
-        
+        if (Input.touchCount > 0)
+        {
+            position = Input.GetTouch(0).position;
+            return true;
+        }
+#if UNITY_EDITOR
+        else if (Input.GetMouseButton(0))
+        {
+            position = Input.mousePosition;
+            return true;
+        }
+#endif
+        else
+        {
+            position = Vector2.zero;
+            return false;
+        }
     }
 }
--- Proyecto Unity/Towersona/Assets/Scripts/Towersona/HOD/Food/Draggable.cs
+++ Proyecto Unity/Towersona/Assets/Scripts/Towersona/HOD/Food/Draggable.cs
@@ -13,114 +13,53 @@ public class Draggable : MonoBehaviour
     private new Transform transform;
 
     private Vector3 touchOffset;
-
     private bool isHeld = false;
-    private bool wasDraggedLastFrame = false;
+
 
     private Vector3 TouchInWorldSpace
     {
         get
         {
-            if (Input.touchCount > 0)
+            if (PointerInput.ReadPosition(out Vector2 touchPosition))
             {
                 return camera.ScreenToWorldPoint(new Vector3(
-                        Input.GetTouch(0).position.x,
-                        Input.GetTouch(0).position.y,
+                        touchPosition.x,
+                        touchPosition.y,
                         transform.position.z - camera.GetComponent<Transform>().position.z));
             }
-
-            //Mouse Controls
-#if UNITY_EDITOR
-            if (Input.GetMouseButton(0))
-            {
-                return camera.ScreenToWorldPoint(new Vector3(
-                        Input.mousePosition.x,
-                        Input.mousePosition.y,
-                        transform.position.z - camera.GetComponent<Transform>().position.z));
-            }
-
-#endif
             else return Vector3.zero;
         }
-        
     }
 
     private void OnMouseDown()
     {
+        Vector3 touchInWorld = TouchInWorldSpace;
+        touchOffset = transform.position - touchInWorld;
 
-#if UNITY_EDITOR
-        if (Input.touchCount > 0 || Input.GetMouseButton(0))
-#else
-        if (Input.touchCount > 0)
-#endif
-        {
-            Vector3 touchInWorld = TouchInWorldSpace;
+        isHeld = true;
 
-            touchOffset = touchInWorld - transform.position;//transform.position - touchInWorld;
-
-            OnDragStart.Invoke();
-            wasDraggedLastFrame = true;
-            isHeld = true;
-        }
-    }
-
-    private void OnMouseDrag()
-    {
-        wasDraggedLastFrame = true;
-        Vector3 tmp = TouchInWorldSpace + touchOffset;
-        transform.position = tmp;
-    }
-
-    private void OnMouseUpAsButton()
-    {
-        if (!isHeld) return;
-        OnLetGo.Invoke();
-        wasDraggedLastFrame = false;
-
-        if (gameObject != null && originalPosition != Vector3.zero)
-        {
-            GoBack();
-        }
-        isHeld = false;
-    }
-
-    private void GoBack()
-    {
-        transform.position = originalPosition;
-        wasDraggedLastFrame = false;
+        OnDragStart.Invoke();
     }
 
     private void Update()
     {
-        if (!wasDraggedLastFrame)
+        if (isHeld)
         {
-            timeWithoutDrag += Time.deltaTime;
-            if (timeWithoutDrag > timeBeforeAutoBack) GoBack();
+            if (PointerInput.IsTouching)
+            {
+                Vector3 tmp = TouchInWorldSpace + touchOffset;
+                transform.position = tmp;
+            }
+            else
+            {
+                isHeld = false;
+                OnLetGo.Invoke();
+            }
         }
-        else
-        {
-            timeWithoutDrag = 0;
-        }
-        wasDraggedLastFrame = false;
-
-#if UNITY_EDITOR
-        if (isHeld && (Input.touchCount == 0 && !Input.GetMouseButton(0)))
-        {
-            OnLetGo.Invoke();
-        }
-#else
-        if (isHeld && Input.touchCount == 0)
-        {
-            OnLetGo.Invoke();
-        }
-#endif
-
-
     }
 
     private void Awake()
     {
         transform = GetComponent<Transform>();
-        originalPosition = transform.position;
     }
 }
