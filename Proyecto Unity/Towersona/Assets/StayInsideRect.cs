7222581f44692e765650c2d595f577812daf521a
Aitor Iribar <aitor.iribar@gmail.com>
2020-01-15 09:56:36 +0100

    La comida no se puede salir de la sección tamagotchi

---
+++ Proyecto Unity/Towersona/Assets/StayInsideRect.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class StayInsideRect : MonoBehaviour
+{
+    public RectTransform rectSource;
+
+    private Vector3[] corners = new Vector3[4];
+
+    private void LateUpdate()
+    {
+        rectSource.GetWorldCorners(corners);
+        Vector3 position = transform.position;
+
+        position.x = Mathf.Clamp(position.x, corners[0].x, corners[2].x);
+        position.y = Mathf.Clamp(position.y, corners[0].y, corners[2].y);
+
+        transform.position = position;
+    }
+}
