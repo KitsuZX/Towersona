d9ee7b7dae129344616b6a918c145966839153b8
Pablo Martínez Domingo <pablokzx@gmail.com>
2020-01-15 19:01:22 +0100

    Empezado el sistema de guardado

---
+++ Proyecto Unity/Towersona/Assets/Scripts/SaveSystem/LevelData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelData : MonoBehaviour
+{
+    public int Index => index;
+    public int Score => score;
+    public bool Available => available;
+
+    private int index;
+    private int score;
+    private bool available;  
+
+    public bool Completed
+    {
+        get
+        {
+            return score != 0;
+        }
+    }
+
+    public LevelData(int index, int score, bool available)
+    {
+        this.index = index;
+        this.score = score;
+        this.available = available;
+    }
+
+    public void SetScore(int score)
+    {
+        this.score = score;
+        SavedData.SaveLevel(this);
+    }
+}
